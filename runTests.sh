7a56409d55e5c2a8a4ea04577b993d51a52755ca
Clovis1444 <eu11111444@gmail.com>
2024-10-14 22:59:56 +0300

    chore: fix runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -15,11 +15,8 @@ for arg in "$@"; do
     fi
 done
 
-# If the first argument is configure
-if [ "$1" = "-c" ] || [ "$1" = "--configure" ]; then
-    # Configure
-    cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=clang -DCMAKE_CXX_COMPILER:FILEPATH=clang++ -S${SCRIPT_DIR} -B${SCRIPT_DIR}\\build -G Ninja || exit 1
-fi
+# Configure
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=clang -DCMAKE_CXX_COMPILER:FILEPATH=clang++ -S${SCRIPT_DIR} -B${SCRIPT_DIR}\\build -G Ninja || exit 1
 # Build
 cmake --build ${SCRIPT_DIR}/build --config ${BUILD_TYPE} --target DLCreatorTests || exit 1
 # Run
